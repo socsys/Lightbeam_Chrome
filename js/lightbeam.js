7501aa30faa17264f6c140051b56a624c4213b4f
rachelkcl <47688352+rachelkcl@users.noreply.github.com>
2021-02-27 10:26:45 +0000

    update sending data only if consent

--- js/lightbeam.js
+++ js/lightbeam.js
@@ -1,145 +1,71 @@
 const lightbeam = {
   websites: {},
-  dataGatheredSince: null,
-  numFirstParties: 0,
-  numThirdParties: 0,
 
   async init() {
     this.websites = await store.getAll();
-    console.log("this websites",this.websites);
-    this.initTPToggle();
-    this.renderGraph();
-    this.addListeners();
-    this.updateVars();
+	this.websitesbackup = await store.getAll();
+   
+	if (localStorage.getItem("consent-notice-user") === null) {
+		
+	document.getElementsByClassName("modal")[0].style.display="";
+	var confirm_btn=document.querySelector("body > main > div > div.modal-footer > button.modal-confirm");
+
+	    confirm_btn.addEventListener('click', async () => {
+			  localStorage['consent-notice-user']=true;
+			  document.getElementById('auto-collection-control').checked=true;
+			  localStorage['auto-collection-control']=true;
+			  document.querySelector("body > main > div ").style.display='none'
+			  this.renderGraph();
+			});
+			
+	var cancel_btn=document.querySelector("body > main > div > div.modal-footer > button.modal-cancel");
+	
+	    cancel_btn.addEventListener('click', async () => {
+			 localStorage['consent-notice-user']=false;
+			 document.querySelector("body > main > div ").style.display='none'
+			 document.getElementById('auto-collection-control').checked=false;
+			 localStorage['auto-collection-control']=false;
+			 this.renderGraph();
+			});
+	}
+	else{
+		this.renderGraph();
+		this.addListeners();
+	}
+	
+	
+	
+	if (document.getElementById('auto-collection-control').checked == true) {
+		if (store.isCollectDate()){
+			document.getElementById('send-data-button').click();
+		}
+	}
+	
   },
 
-  async initTPToggle() {
-    const toggleCheckbox
-      = document.getElementById('tracking-protection-control');
-    const trackingProtection = document.getElementById('tracking-protection');
-    const trackingProtectionDisabled
-      = document.getElementById('tracking-protection-disabled');
-    // Do we support setting TP
-    if ('trackingProtectionMode' in chrome.privacy.websites) {
-      trackingProtection.hidden = false;
-      trackingProtectionDisabled.hidden = true;
-
-      const trackingProtectionState
-        = await chrome.privacy.websites.trackingProtectionMode.get({});
-      let value = true;
-      if (trackingProtectionState.value !== 'always') {
-        value = false;
-      }
-      toggleCheckbox.checked = value;
-      toggleCheckbox.addEventListener('change', () => {
-        const value = toggleCheckbox.checked ? 'always' : 'private_browsing';
-        chrome.privacy.websites.trackingProtectionMode.set({ value });
-      });
-    } else {
-      trackingProtection.hidden = true;
-      trackingProtectionDisabled.hidden = false;
-    }
-  },
+  
+ 
 
   renderGraph() {
     const transformedData = this.transformData();
     viz.init(transformedData.nodes, transformedData.links);
+	
+	
+	
   },
 
   addListeners() {
     this.downloadData();
-	//this.sendData();
-    this.downloadData1();
-    this.downloadCookie();
-	//this.resetCookie();
+	this.sendData();
 	this.downloadCategory();
     this.resetData();
-	//this.file();
 	this.mailsome1();
     storeChild.onUpdate((data) => {
       this.redraw(data);
     });
   },
 
-  // Called from init() (isFirstParty = undefined)
-  // and redraw() (isFirstParty = true or false).
-  async updateVars(isFirstParty) {
-
-    // initialize dynamic vars from storage
-    if (!this.dataGatheredSince) {
-      const { dateStr, fullDateTime } = await this.getDataGatheredSince();
-      if (!dateStr) {
-        return;
-      }
-      this.dataGatheredSince = dateStr;
-      const dataGatheredSinceElement
-        = document.getElementById('data-gathered-since');
-      dataGatheredSinceElement.textContent = this.dataGatheredSince || '';
-      dataGatheredSinceElement.setAttribute('datetime', fullDateTime || '');
-    }
-    if (isFirstParty === undefined) {
-      this.numFirstParties = await this.getNumFirstParties();
-      this.setPartyVar('firstParty');
-      this.numThirdParties = await this.getNumThirdParties();
-      this.setPartyVar('thirdParty');
-      return;
-    }
-
-    // update on redraw
-    if (isFirstParty) {
-      this.numFirstParties++;
-      this.setPartyVar('firstParty');
-    } else {
-      this.numThirdParties++;
-      this.setPartyVar('thirdParty');
-    }
-  },
-
-  // Updates dynamic variable values in the page
-  setPartyVar(party) {
-    const numFirstPartiesElement = document.getElementById('num-first-parties');
-    const numThirdPartiesElement = document.getElementById('num-third-parties');
-    if (party === 'firstParty') {
-      if (this.numFirstParties === 0) {
-        numFirstPartiesElement.textContent = '';
-      } else {
-        numFirstPartiesElement.textContent = `${this.numFirstParties} Sites`;
-      }
-    } else if (this.numThirdParties === 0) {
-      numThirdPartiesElement.textContent = '';
-    } else {
-      const str = `${this.numThirdParties} Third Party Sites`;
-      numThirdPartiesElement.textContent = str;
-    }
-  },
-
-  async getDataGatheredSince() {
-    const firstRequestUnixTime = await storeChild.getFirstRequestTime();
-    if (!firstRequestUnixTime) {
-      return {};
-    }
-    // reformat unix time
-    let fullDateTime = new Date(firstRequestUnixTime);
-    let dateStr = fullDateTime.toDateString();
-    // remove day of the week
-    const dateArr = dateStr.split(' ');
-    dateArr.shift();
-    dateStr = dateArr.join(' ');
-    // ISO string used for datetime attribute on <time>
-    fullDateTime = fullDateTime.toISOString();
-    return {
-      dateStr,
-      fullDateTime
-    };
-  },
-
-  async getNumFirstParties() {
-    return await storeChild.getNumFirstParties();
-  },
-
-  async getNumThirdParties() {
-    return await storeChild.getNumThirdParties();
-  },
+  
 
   transformData() {
     const nodes = [];
@@ -166,89 +92,55 @@ const lightbeam = {
   
 
   downloadData()  {
-	  
-	
-	
-	
     const saveData = document.getElementById('save-data-button');
-    saveData.addEventListener('click', async () => {
-      const data = await store.getAll();
-      const blob = new Blob([JSON.stringify(data ,' ' , 2)],
-        {type : 'application/json'});
-      const url = window.URL.createObjectURL(blob);
-      const downloading = chrome.downloads.download({
-        url : url,
-        filename : 'lightbeamData.json',
-        conflictAction : 'uniquify'
-      });
-      await downloading;
+	const outputdata = this.websitesbackup;
+	function filterObj(obj, arr) {
+		if (typeof (obj) !== "object" || !Array.isArray(arr)) {
+			throw new Error("Wrong Format")
+		}
+		const result = {}
+		Object.keys(obj).filter((key) => arr.includes(key)).forEach((key) => {
+			result[key] = obj[key]
+		})
+		return result
+	}
 
-		
-		
-    });
-  },
-  
-
-  downloadData1() {
-    const saveData = document.getElementById('save-data-button1');
+	require_keys = ['hostname', 'firstPartyHostnames', 'firstParty', 'thirdParties'];
+	Downloaddata={};
+	for (var n in outputdata) {
+        ObjDown=outputdata[n];
+		Downloaddata[n]=filterObj(outputdata[n],require_keys)
+	}
+	outputdata['__UUID']=store.transUUID();
     saveData.addEventListener('click', async () => {
-      const data = await store.getAll1();
-      const blob = new Blob([JSON.stringify(data ,' ' , 2)],
-        {type : 'application/json'});
-      const url = window.URL.createObjectURL(blob);
-      const downloading = chrome.downloads.download({
-        url : url,
-        filename : 'DuplicateFistParties.json',
-        conflictAction : 'uniquify'
-      });
-      await downloading;
-    });
-  },
-
-  
-  downloadCookie() {
-    const saveData = document.getElementById('cookie-button');
-    saveData.addEventListener('click', async () => {
-	  //const data = await store.getCookie();
-	  const data = decodeURIComponent(document.cookie);
-      const blob = new Blob([JSON.stringify(data ,' ' , 2)],
-        {type : 'application/json'});
-      const url = window.URL.createObjectURL(blob);
-	  var CookieData=JSON.stringify(data)
-	  
-	  const msgBegin = 'Pressing OK to view your cookies and send us for research support (encrypted)';
-      const msgEnd = 'Are you sure?';
-      const confirmation = confirm(`${msgBegin + msgEnd}`);
-      if (confirmation) {
-    const downloading = chrome.downloads.download({
-        url : url,
-        filename : 'Cookie.json',
-        conflictAction : 'uniquify'
-      });
-      //await downloading;
-	  var CookieData=JSON.stringify(data)
-	  var xhr = new XMLHttpRequest(); 
-	  xhr.open("POST", "********.php", true);
-	  xhr.send(JSON.stringify(CookieData));
-	  await alert("Cookie: \n" + CookieData + ".");
-	  
+		if (localStorage.getItem("DiffResult_Weekly") !== null) {
+		  const DifferDetaildata = JSON.parse(localStorage["DiffResult_Weekly"]);
+		var tmp = {}
+		tmp['__UUID']=store.transUUID();
+		DifferDetaildata.push(tmp);
+			//send to server if consenting
+		  localStorage.removeItem("DiffResult_Weekly"); 
 	  }
+	var CurrentDate = (new Date(new Date().getTime())).toJSON().split('T')[0];
+	localStorage["Data_Collection_Date"]=CurrentDate;
+	const blob_download = new Blob([JSON.stringify(Downloaddata,' ' , 2)], 
+		{type : 'application/json'});
+      const downloadurl = window.URL.createObjectURL(blob_download);
+      const downloading = chrome.downloads.download({
+        url : downloadurl,
+        filename : 'ThunderbeamData.json',
+        conflictAction : 'uniquify'
+      });
+      await downloading;
+	  
+	 document.getElementById('reset-data-button').style.backgroundColor='#194419';
+	 document.getElementById('reset-data-button').innerText='Reset Data (Recommended)';
     });
+	
+
   },
   
-    resetCookie() {
-    const resetData = document.getElementById('cookie-delete-button');
-    resetData.addEventListener('click', async () => {
-      const msgBegin = 'Pressing OK will delete all Cookies storage. ';
-      const msgEnd = 'Are you sure?';
-      const confirmation = confirm(`${msgBegin + msgEnd}`);
-      if (confirmation) {
-        //await store.deleteCookie();
-		await store.deleteAllCookies();
-        window.location.reload();
-      }
-    });
-  },
+
   
   downloadCategory() {
     const saveData = document.getElementById('hash-button');
@@ -272,13 +164,7 @@ const lightbeam = {
     const resetData = document.getElementById('reset-data-button');
     resetData.addEventListener('click', async () => {
 		
-	const data = await store.getAll();
-	  var xhr = new XMLHttpRequest(); 
-	  xhr.open("POST", ""********..php", true);
-	  xhr.send(JSON.stringify(data));
-
-	  
-      const msgBegin = 'Pressing OK will delete all data but backup the encryped version to support our research.';
+      const msgBegin = 'Pressing OK will delete all data.';
       const msgEnd = 'Are you sure?';
       const confirmation = confirm(`${msgBegin + msgEnd}`);
       if (confirmation) {
@@ -286,6 +172,60 @@ const lightbeam = {
         await storeChild.reset();
         window.location.reload();
       }
+	  
+    });
+  },
+  
+  
+    sendData() {
+	const outputdata = this.websitesbackup;
+	outputdata['__UUID']=store.transUUID();
+
+			  
+	
+    const sendData = document.getElementById('send-data-button');
+	
+    sendData.addEventListener('click', async () => {
+	  if (localStorage.getItem("DiffResult_Weekly") !== null) {
+		  const DifferDetaildata = JSON.parse(localStorage["DiffResult_Weekly"]);
+			var tmp = {}
+			tmp['__UUID']=store.transUUID();
+			DifferDetaildata.push(tmp);
+		 //send to server if consenting
+		  localStorage.removeItem("DiffResult_Weekly"); 
+	  }
+	  var CurrentDate = (new Date(new Date().getTime())).toJSON().split('T')[0];
+	  localStorage["Data_Collection_Date"]=CurrentDate;
+	  
+	  if ((localStorage.getItem("consent-notice-user") === null)||(localStorage['auto-collection-control']=='false')) {
+		
+		const msgBegin = 'This extension is developed as part of a large study of web tracking. \n * We wish to collect your data anonymously to help understand tracking around the world. If you agree, please press OK below. \n * If you press NO, then your data will NOT be collected, but you will still be able to use our extension. \n * You can change your mind at any time by simply changing the auto data collect setting on the left.';
+		const msgEnd = ' ';
+	  
+		const confirmation = confirm(`${msgBegin + msgEnd}`);
+		if (confirmation) {
+			localStorage['consent-notice-user']=true;
+			document.getElementById('auto-collection-control').checked=true;
+			 localStorage['auto-collection-control']=true;
+			//not turn to research ethics page again
+				/*var n=confirm("Pressing OK to confirm you above 16 years old before sending the data.");
+				if(n==true){
+				  var x=prompt("If you want to know the criteria for the consent process reviewed by the Research Ethics Committee of King's College London","https://www.kcl.ac.uk/research/support/rgei/research-ethics/guidelines-for-external-researchers");
+				  window.open("https://www.kcl.ac.uk/research/support/rgei/research-ethics/guidelines-for-external-researchers","_blank","top=50,left=80,width=400,height=500,scrollbar=yes,statusbar=no,menubar=no,toolbar=no"); 
+				}*/
+			alert("Thank you for your support !")
+		  }
+		else {
+			 
+			 document.getElementById('auto-collection-control').checked=false;
+			 localStorage['auto-collection-control']=false;
+			 localStorage['consent-notice-user']=false;
+			 await alert("\n You have cancelled data support. \n \n If you change your mind and decide to support our data research, please use the toggle switch to opt in or back to \"Send Data\" and select \"OK\":)\n ");
+		  }
+	  }
+	  else{
+		  alert("Thank you for your support !")
+	  }
     });
   },
   
@@ -300,14 +240,11 @@ const lightbeam = {
 	},
 
 
-
   redraw(data) {
     if (!(data.hostname in this.websites)) {
       this.websites[data.hostname] = data;
-      this.updateVars(data.firstParty);
     }
     if (data.firstPartyHostnames) {
-      // if we have the first parties make the link if they don't exist
       data.firstPartyHostnames.forEach((firstPartyHostname) => {
         if (this.websites[firstPartyHostname]) {
           const firstPartyWebsite = this.websites[firstPartyHostname];
