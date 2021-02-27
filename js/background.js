192d6d858f8dbd9115027e92176ee1c89b48b330
rachelkcl <47688352+rachelkcl@users.noreply.github.com>
2021-02-27 10:30:16 +0000

    update firstRun check

--- js/background.js
+++ js/background.js
@@ -1,18 +1,17 @@
 'use strict';
-/*
-async function checkFirstRun() {
 
+async function checkFirstRun() {
   if (await store.isFirstRun()) {
     chrome.tabs.create({ url: 'first-run.html' });
+	//user data privacy
   }
 
 }
 
 checkFirstRun();
-*/
+
 async function runLightbeam() {
-  // Checks to see if Lightbeam is already open.
-  // Returns true if it is, false if not.
+	
   async function isOpen() {
     console.log("isopen");
     const tabs = await chrome.tabs.query({});
@@ -27,11 +26,14 @@ async function runLightbeam() {
   if (!lightbeamTab) {
     // only open a new Lightbeam instance if one isn't already open.
     chrome.tabs.create({ url: 'index.html' });
+	
   } else if (!lightbeamTab.active) {
-     // re-focus Lightbeam if it is already open but lost focus
     chrome.tabs.update(lightbeamTab.id, {active: true});
   }
+
 }
 
-// When the user clicks browserAction icon in toolbar, run Lightbeam
 chrome.browserAction.onClicked.addListener(runLightbeam);
+
+
+
